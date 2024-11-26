cf605dd947a479ac6ec0f5834e4a19b3bf359b5d
vietcoi2k3 <95207717+vietcoi2k3@users.noreply.github.com>
2024-11-26 22:46:58 +0700

    Update common.ts

--- admin-tlu-food/src/constants/common.ts
+++ admin-tlu-food/src/constants/common.ts
@@ -1,2 +1,2 @@
-export const STATIC_HOST = "http://128.199.208.76:8081/"
-export const SOCKET_URL = "http://128.199.208.76:8081/ws-iotfood"
+export const STATIC_HOST = "http://152.42.180.200:8081/"
+export const SOCKET_URL = "http://152.42.180.200:8081/ws-iotfood"
