53c7dc0c1931033773a422a055107071730e6e3a
vietcoi2k3 <95207717+vietcoi2k3@users.noreply.github.com>
2024-11-26 22:47:53 +0700

    Update common.ts

--- IotFoodCore_TLU/src/constants/common.ts
+++ IotFoodCore_TLU/src/constants/common.ts
@@ -1,3 +1,3 @@
-export const STATIC_HOST = "http://128.199.208.76:8081/"
-export const SOCKET_URL = "http://128.199.208.76:8081/ws-iotfood"
-export const AI_URL = "https://128.199.208.76:8081/"
+export const STATIC_HOST = "http://152.42.180.200:8081/"
+export const SOCKET_URL = "http://152.42.180.200:8081/ws-iotfood"
+export const AI_URL = "https://152.42.180.200:8081/"
